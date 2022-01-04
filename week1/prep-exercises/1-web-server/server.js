08fae90241074e33bb7a7d1066ec5bc74e391042
Lydia Samir <L-Samir@hotmail.com>
2022-01-04 10:47:10 +0100

    added prep and practice exercises week1

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,31 @@
  */
 
 const http = require('http');
-
+const fs = require('fs');
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === '/') {
+    fs.readFile('./index.html', (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(content);
+    });
+  }
+  if (req.url === '/index.js') {
+    fs.readFile('./index.js', (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-type': 'text/javascript' });
+      res.end(content);
+    });
+  }
+  if (req.url === '/style.css') {
+    fs.readFile('./style.css', (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-type': 'text/css' });
+      res.end(content);
+    });
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
